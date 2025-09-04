6c5d598f20ddcf17c1177be8ba4cd5302865f81c
AhmedElesawy <elesawy928@gmail.com>
2025-09-04 16:09:44 +0300

    device unit to know the platform

---
+++ lib/core/utils/device_utils.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:shoppe/core/utils/size_helper_extention.dart';
+
+class DeviceUtils {
+  static bool isMobile(BuildContext context) => context.screenWidth < 600;
+
+  static bool isTablet(BuildContext context) =>
+      context.screenWidth >= 600 && context.screenWidth < 1200;
+
+  static bool isDesktop(BuildContext context) => context.screenWidth >= 1200;
+
+  static T valueDecider<T>(
+    BuildContext context, {
+    required T onMobile,
+    T? onTablet,
+    T? onDesktop,
+    T? onOther,
+  }) {
+    if (isMobile(context)) {
+      return onMobile;
+    } else if (isTablet(context) && onTablet != null) {
+      return onTablet;
+    } else if (isDesktop(context) && onDesktop != null) {
+      return onDesktop;
+    } else {
+      return onOther ?? onMobile;
+    }
+  }
+}
