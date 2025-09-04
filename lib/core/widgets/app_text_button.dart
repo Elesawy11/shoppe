925867c0ac8a6f6327d44ac8ea0f48f72c729ef6
AhmedElesawy <elesawy928@gmail.com>
2025-09-04 16:11:35 +0300

    custom text button widget

--- lib/core/widgets/app_text_button.dart
+++ lib/core/widgets/app_text_button.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+// import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shoppe/core/utils/size_helper_extention.dart';
+import '../themes/app_color.dart';
 import '../utils/styles.dart';
 
 class AppTextButton extends StatelessWidget {
@@ -22,21 +24,27 @@ class AppTextButton extends StatelessWidget {
   final double? height;
   @override
   Widget build(BuildContext context) {
+    
     return TextButton(
       onPressed: onPressed,
       style: TextButton.styleFrom(
         overlayColor: Colors.black.withValues(),
-        backgroundColor: backgroundColor ?? Colors.black,
-        minimumSize: Size(width ?? double.infinity, height ?? 52.h),
+        backgroundColor: backgroundColor ?? AppColor.primary,
+        minimumSize: Size(
+          width ?? double.infinity,
+          height ?? context.setHeight(52),
+        ),
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(16.r),
+          borderRadius: BorderRadius.circular(context.setMinSize(16)),
         ),
       ),
       child:
           child ??
           Text(
             text,
-            style: Styles.button.copyWith(color: textColor ?? Colors.white),
+            style: context.button.copyWith(
+              color: textColor ?? AppColor.white,
+            ),
           ),
     );
   }
